0c4d103053e90817f91ca2aa59a9d866eab4f46b
lqz <liqingzhu@pingpukj.com>
2023-05-26 16:57:14 +0800

    INIT

---
+++ 3-AppService/java/Scannner/src/main/java/com/lqz/study/MyScanner.java
@@ -0,0 +1,15 @@
+package com.lqz.study;
+
+import java.util.Scanner;
+
+/**
+ * @author Qing
+ * @create  2022-10-23 17:27
+ */
+public class MyScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(num);
+    }
+}
---
+++ 3-AppService/java/Scannner/src/main/java/com/lqz/study/MySwitch.java
@@ -0,0 +1,28 @@
+package com.lqz.study;
+
+/**
+ * @author Qing
+ * @create 2022-10-23 17:37
+ */
+public class MySwitch {
+
+    public static void main(String[] args) {
+        int number = 0;
+        switch (number)
+        {
+            case 0 :
+                System.out.println("0");
+                break;
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("other");
+                break;
+        }
+
+    }
+}
